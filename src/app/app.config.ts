4815eb111cceac3fa988ceb58164dc6a3753d705
nreenleesya <2023406908@student.uitm.edu.my>
2025-01-07 11:14:22 +0800

    chat service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,14 +11,6 @@ import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebase)),
-      provideFirestore(() => getFirestore()),
-      provideAuth(() => getAuth()),
-      provideFunctions(() => getFunctions()),
-      provideStorage(() => getStorage()),
-      provideMessaging(() => getMessaging())
-    ),
-    provideRouter(routes)
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-d0f26","appId":"1:9078663734:web:fdbb869f3bda5279dac697","storageBucket":"friendlychat-d0f26.firebasestorage.app","apiKey":"AIzaSyA_13aQCnkHf3h9kX4QqjJVxCtxUT9BXBM","authDomain":"friendlychat-d0f26.firebaseapp.com","messagingSenderId":"9078663734"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage()), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-d0f26","appId":"1:9078663734:web:fdbb869f3bda5279dac697","storageBucket":"friendlychat-d0f26.firebasestorage.app","apiKey":"AIzaSyA_13aQCnkHf3h9kX4QqjJVxCtxUT9BXBM","authDomain":"friendlychat-d0f26.firebaseapp.com","messagingSenderId":"9078663734"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage()), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-d0f26","appId":"1:9078663734:web:fdbb869f3bda5279dac697","storageBucket":"friendlychat-d0f26.firebasestorage.app","apiKey":"AIzaSyA_13aQCnkHf3h9kX4QqjJVxCtxUT9BXBM","authDomain":"friendlychat-d0f26.firebaseapp.com","messagingSenderId":"9078663734"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
   ],
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,11 @@
 export const environment = {
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
+    apiKey: "AIzaSyAZ4o362QbF7Q_V_QiytSvDnhpmrLq3_2M",
+    authDomain: "friendlychat-e0f4b.firebaseapp.com",
+    projectId: "friendlychat-e0f4b",
+    storageBucket: "friendlychat-e0f4b.firebasestorage.app",
+    messagingSenderId: "291076257964",
+    appId: "1:291076257964:web:f23f6584697b1dbad68709",
     measurementId: 'G-MEASUREMENT_ID',
   },
 };
